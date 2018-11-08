d8ffa7aa4528c5b43f33a9b0b7689f1122f46a8f
lingbeijin <beijing.ling@lavainternational.com>
2018-11-08 10:56:04 +0800

    [master] [Optimize] [DESC]:[完善banner界面、主页数据获取]

--- app/src/main/java/com/lava/music/data/network/model/Banner.java
+++ app/src/main/java/com/lava/music/data/network/model/Banner.java
@@ -60,6 +60,227 @@ public class Banner {
         private List<Item> mBanners;
 
 
+        @Expose
+        @SerializedName("channel")
+        private List<Channel> mChannels;
+
+        @Expose
+        @SerializedName("newGoodsList")
+        private List<NewGoods> mNewGoodsList;
+
+        @Expose
+        @SerializedName("hotGoodsList")
+        private List<HotGoods> mHotGoodsList;
+
+        @Expose
+        @SerializedName("brandList")
+        private List<Brand> mBrandList;
+
+        @Expose
+        @SerializedName("topicList")
+        private List<Topic> mTopicList;
+
+
+//        @Expose
+//        @SerializedName("brandList")
+//        private List<Topic> mTopicList;
+    }
+
+//    public static class Topic {
+//
+//    }
+    public static class Topic {
+        @Expose
+        @SerializedName("id")
+        private int ID;
+
+        @Expose
+        @SerializedName("title")
+        private String title;
+
+        @Expose
+        @SerializedName("content")
+        private String content;
+
+        @Expose
+        @SerializedName("avatar")
+        private String avatar;
+
+        @Expose
+        @SerializedName("item_pic_url")
+        private String item_pic_url;
+
+        @Expose
+        @SerializedName("subtitle")
+        private String subtitle;
+
+        @Expose
+        @SerializedName("topic_category_id")
+        private int topic_category_id;
+
+        @Expose
+        @SerializedName("price_info")
+        private float price_info;
+
+        @Expose
+        @SerializedName("read_count")
+        private String read_count;
+
+        @Expose
+        @SerializedName("scene_pic_url")
+        private String scene_pic_url;
+
+        @Expose
+        @SerializedName("topic_template_id")
+        private int topic_template_id;
+
+        @Expose
+        @SerializedName("topic_tag_id")
+        private int topic_tag_id;
+
+        @Expose
+        @SerializedName("sort_order")
+        private int sort_order;
+
+        @Expose
+        @SerializedName("is_show")
+        private int is_show;
+
+    }
+
+    public static class Brand {
+
+        @Expose
+        @SerializedName("id")
+        private int ID;
+
+        @Expose
+        @SerializedName("name")
+        private String mName;
+
+        @Expose
+        @SerializedName("simple_desc")
+        private String mSimpleDesc;
+
+        @Expose
+        @SerializedName("pic_url")
+        private String pic_url;
+
+        @Expose
+        @SerializedName("sort_order")
+        private int sort_order;
+
+        @Expose
+        @SerializedName("is_show")
+        private int is_show;
+
+        @Expose
+        @SerializedName("floor_price")
+        private float floor_price;
+
+        @Expose
+        @SerializedName("app_list_pic_url")
+        private String app_list_pic_url;
+
+        @Expose
+        @SerializedName("new_pic_url")
+        private String new_pic_url;
+
+        @Expose
+        @SerializedName("new_sort_order")
+        private int new_sort_order;
+
+    }
+
+    public static class HotGoods extends NewGoods {
+
+        @Expose
+        @SerializedName("goods_brief")
+        private String mGoodsBrief;
+
+        public void setGoodsBrief(String mGoodsBrief) {
+            this.mGoodsBrief = mGoodsBrief;
+        }
+    }
+
+    public static class NewGoods {
+
+        @Expose
+        @SerializedName("id")
+        private int ID;
+
+        @Expose
+        @SerializedName("name")
+        private String mName;
+
+
+        @Expose
+        @SerializedName("retail_price")
+        private int mRetailPrice;
+
+        @Expose
+        @SerializedName("list_pic_url")
+        private String mPicUrl;
+
+        public int getID() {
+            return ID;
+        }
+
+        public String getName() {
+            return mName;
+        }
+
+        public int getRetailPrice() {
+            return mRetailPrice;
+        }
+
+        public String getPicUrl() {
+            return mPicUrl;
+        }
+    }
+
+    public static class Channel {
+
+        @Expose
+        @SerializedName("id")
+        private int ID;
+
+        @Expose
+        @SerializedName("name")
+        private String mName;
+
+
+        @Expose
+        @SerializedName("url")
+        private String mUrl;
+
+        @Expose
+        @SerializedName("icon_url")
+        private String mImageUrl;
+
+        @Expose
+        @SerializedName("sort_order")
+        private int mSortOrder;
+
+        public void setID(int ID) {
+            this.ID = ID;
+        }
+
+        public void setName(String mName) {
+            this.mName = mName;
+        }
+
+        public void setUrl(String mUrl) {
+            this.mUrl = mUrl;
+        }
+
+        public void setImageUrl(String mImageUrl) {
+            this.mImageUrl = mImageUrl;
+        }
+
+        public void setSortOrder(int mSortOrder) {
+            this.mSortOrder = mSortOrder;
+        }
     }
 
     public static class Item {
